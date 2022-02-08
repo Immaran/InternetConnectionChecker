7f8df980d3749a9d26c1107c2c5700c7a3d2a262
Mateusz Żukowski <74957@student.pb.edu.pl>
2022-02-08 17:27:55 +0100

    Done

--- InternetConnectionChecker/MainWindow.xaml.cs
+++ InternetConnectionChecker/MainWindow.xaml.cs
@@ -1,28 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.NetworkInformation;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace InternetConnectionChecker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private double _frequency;
+
+        private string _ip;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _frequency = Properties.Settings.Default.Frequency;
+
+            _ip = Properties.Settings.Default.IP;
+
+            DispatcherTimer timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1 / _frequency);
+            timer.Tick += Check;
+            timer.Start();
+        }
+
+        private void Check(object sender, EventArgs e)
+        {
+            if (this.IsLoaded)
+            {
+                try
+                {
+                    if (new Ping().Send(_ip).Status.ToString().Equals("Success"))
+                    {
+                        root.Background = Brushes.Green;
+                        textBlock.Foreground = Brushes.White;
+                        textBlock.Text = "INTERNET IS ON!";
+                    }
+                }
+                catch (PingException)
+                {
+                    root.Background = Brushes.Red;
+                    textBlock.Foreground = Brushes.Black;
+                    textBlock.Text = "INTERNET IS OFF!";
+                }
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            textBlock.FontSize = this.Width / 10;
         }
     }
 }
--- InternetConnectionChecker/Properties/Settings.Designer.cs
+++ InternetConnectionChecker/Properties/Settings.Designer.cs
@@ -8,21 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace InternetConnectionChecker.Properties
-{
+namespace InternetConnectionChecker.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public double Frequency {
+            get {
+                return ((double)(this["Frequency"]));
+            }
+            set {
+                this["Frequency"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("8.8.8.8")]
+        public string IP {
+            get {
+                return ((string)(this["IP"]));
+            }
+            set {
+                this["IP"] = value;
             }
         }
     }
